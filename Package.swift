f40b0cd80b20e2615e5262469687dc79a9b98886
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2025-07-10 09:03:57 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .target(
             name: "YeeKit",
-            path: "Sources/"
+            path: "Sources/YeeKit"
         )
     ]
 )
--- Sources/YeeKit/Extension/YeeEncodableExtension.swift
+++ Sources/YeeKit/Extension/YeeEncodableExtension.swift
@@ -11,7 +11,7 @@ import Foundation
 import Combine
 import SwiftUI
 
-extension Encodable {
+public extension Encodable {
     func toDictionary() -> [String: Any]? {
         do {
             let encoder = JSONEncoder()
