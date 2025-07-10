2916e1a40a0613da6192f92750f3e551f62271a2
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2025-07-10 09:36:37 +0800

    release: 1.0.7

--- Sources/YeeKit/Tool/YeeLocationTool.swift
+++ Sources/YeeKit/Tool/YeeLocationTool.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreLocation
 
 public class LocationTool: NSObject {
-    static let shared = LocationTool()
+    public static let shared = LocationTool()
     
     private let locationManager = CLLocationManager()
     private var completion: ((CLLocationCoordinate2D?, Error?) -> Void)?
