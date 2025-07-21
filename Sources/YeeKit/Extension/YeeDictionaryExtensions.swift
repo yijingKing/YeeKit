670f3aa8bcbe140a2dcf56e24a5f94fdb761d2a9
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2025-07-21 17:30:51 +0800

    release: 1.1.1

--- Sources/YeeKit/Extension/YeeDictionaryExtensions.swift
+++ Sources/YeeKit/Extension/YeeDictionaryExtensions.swift
@@ -33,6 +33,7 @@ public extension Dictionary where Key == String, Value == Any {
         }
     }
 }
+
 public extension Dictionary {
     func compactMapKeys<T: Hashable>(_ transform: (Key) throws -> T?) rethrows -> [T: Value] {
         var result = [T: Value]()
@@ -42,5 +43,64 @@ public extension Dictionary {
             }
         }
         return result
+    }
+}
+public extension Dictionary where Key == String {
+    /// 泛型安全取值
+    func value<T>(forKey key: String, as type: T.Type) -> T? {
+        return self[key] as? T
+    }
+    /// 安全地从字典中取出字符串值
+    func stringValue(forKey key: String) -> String? {
+        guard let value = self[key] else { return nil }
+        if let str = value as? String {
+            return str
+        } else if let num = value as? NSNumber {
+            return num.stringValue
+        } else if let any = value as? CustomStringConvertible {
+            return any.description
+        }
+        return nil
+    }
+    /// 安全取出 Bool 值
+    func boolValue(forKey key: String) -> Bool? {
+        if let value = self[key] {
+            if let b = value as? Bool {
+                return b
+            } else if let str = value as? String {
+                return (str as NSString).boolValue
+            } else if let num = value as? NSNumber {
+                return num.boolValue
+            }
+        }
+        return nil
+    }
+
+    /// 安全取出 Int 值
+    func intValue(forKey key: String) -> Int? {
+        if let value = self[key] {
+            if let i = value as? Int {
+                return i
+            } else if let str = value as? String {
+                return Int(str)
+            } else if let num = value as? NSNumber {
+                return num.intValue
+            }
+        }
+        return nil
+    }
+
+    /// 安全取出 Double 值
+    func doubleValue(forKey key: String) -> Double? {
+        if let value = self[key] {
+            if let d = value as? Double {
+                return d
+            } else if let str = value as? String {
+                return Double(str)
+            } else if let num = value as? NSNumber {
+                return num.doubleValue
+            }
+        }
+        return nil
     }
 }
