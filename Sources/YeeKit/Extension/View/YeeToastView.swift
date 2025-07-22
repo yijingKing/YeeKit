71152a02c91a863dd29dc52cd8af39e4776500ab
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2025-07-22 17:40:12 +0800

    release: 1.1.8

--- Sources/YeeKit/Extension/View/YeeToastView.swift
+++ Sources/YeeKit/Extension/View/YeeToastView.swift
@@ -18,7 +18,7 @@ public struct YeeToastConfig : Sendable{
     var foregroundColor: Color = .white
     var maskColor: Color = Color.black.opacity(0)
     var duration: Double = 1.5
-    
+    var loadingDuration: Double = 99 // 新增
     // 全局默认配置
     public static let `default` = YeeToastConfig()
 }
@@ -78,7 +78,7 @@ private struct ToastLoadingView: View {
                 .cornerRadius(10)
                 .transition(.opacity)
                 .onAppear {
-                    let duration = dismissAfter ?? config.duration
+                    let duration = dismissAfter ?? config.loadingDuration
                     if dismissAfter != nil || dismissAfter == nil {
                         // 仅当 dismissAfter 有值或使用默认配置时自动消失
                         DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
@@ -132,22 +132,112 @@ private struct ToastTextView: View {
         var body: some View {
             VStack(spacing: 20) {
                 Button("显示文本 Toast") {
-                    isShowingTextToast = true
-                    isShowingLoadingToast = false
-
+                    YeeToastManager.shared.show(text: "这是全局 Toast")
                 }
                 Button("显示加载中 Toast") {
-                isShowingTextToast = false
-                    isShowingLoadingToast = true
+                    YeeToastManager.shared.showLoading()
                 }
                 .padding(.top,100)
             }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .ignoresSafeArea()
-            .toast("这是一个提示", isPresented: $isShowingTextToast)
-            .toastLoading(isPresented: $isShowingLoadingToast, dismissAfter: 99999)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea()
+            .yeeToastSupport()
+            .environmentObject(YeeToastManager.shared)
         }
     }
-    
     return ToastPreview()
+}
+
+
+public class YeeToastManager: ObservableObject {
+    @MainActor static let shared = YeeToastManager()
+    
+    @Published var text: String?
+    @Published var isLoading = false
+
+    private init() {}
+
+    @MainActor
+    func show(text: String, duration: Double? = nil) {
+        self.text = text
+        hideLoading()
+        DispatchQueue.main.asyncAfter(deadline: .now() + (duration ?? YeeToastConfig.default.duration)) {
+            self.text = nil
+        }
+    }
+
+    /// 显示加载中
+    @MainActor
+    func showLoading() {
+        self.text = nil
+        isLoading = true
+    }
+
+    @MainActor
+    func hideLoading() {
+        self.text = nil
+        isLoading = false
+    }
+}
+
+private struct GlobalToastTextView: View {
+    @EnvironmentObject var toast: YeeToastManager
+    @Environment(\.toastConfig) var config
+    
+    var body: some View {
+        Group {
+            if let text = toast.text {
+                ZStack {
+                    config.maskColor
+                        .ignoresSafeArea()
+                        .contentShape(Rectangle())
+                        .onTapGesture {}
+                    
+                    Text(text)
+                        .font(.system(size: 18))
+                        .padding(30)
+                        .background(config.backgroundColor)
+                        .foregroundColor(config.foregroundColor)
+                        .cornerRadius(8)
+                        .transition(.opacity)
+                }
+            }
+        }
+    }
+}
+
+private struct GlobalToastLoadingView: View {
+    @EnvironmentObject var toast: YeeToastManager
+    @Environment(\.toastConfig) var config
+
+    var body: some View {
+        Group {
+            if toast.isLoading {
+                ZStack {
+                    config.maskColor
+                        .ignoresSafeArea()
+                        .contentShape(Rectangle())
+                        .onTapGesture {}
+
+                    HStack(spacing: 10) {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: config.foregroundColor))
+                            .scaleEffect(2)
+                    }
+                    .padding(50)
+                    .background(config.backgroundColor)
+                    .cornerRadius(10)
+                    .transition(.opacity)
+                }
+            }
+        }
+    }
+}
+
+public extension View {
+    func yeeToastSupport() -> some View {
+        self
+            .overlay(GlobalToastTextView().environmentObject(YeeToastManager.shared))
+            .overlay(GlobalToastLoadingView().environmentObject(YeeToastManager.shared))
+    }
 }
